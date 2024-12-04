0e6d86a34191627241b2a30f4b007393726c4fa0
Clinton-Gilly <sungurclinton@gmail.com>
2024-12-04 10:47:02 +0300

    12qw

--- fileshare/settings.py
+++ fileshare/settings.py
@@ -104,6 +104,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (user uploads)
 MEDIA_URL = '/media/'
